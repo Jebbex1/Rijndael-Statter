f8b8910ce6d6a96b98632dd0133ca8c2f40458dd
Jebbex <107323805+Jebbex1@users.noreply.github.com>
2025-07-15 11:18:45 +0300

    Added mix columns functions for all layers and circular tests, added Numba acceleration where possible

--- rijndael_statter/block.py
+++ rijndael_statter/block.py
@@ -1,37 +1,37 @@
 import numpy as np
-from .constants import S_BOX, INVERSE_S_BOX
+from .constants import S_BOX, INVERSE_S_BOX, EXTENTION_FIELD, XY_MULT_MATRIX, INVERSE_XY_MULT_MATRIX, XZ_MULT_MATRIX, \
+    INVERSE_XZ_MULT_MATRIX, YZ_MULT_MATRIX, INVERSE_YZ_MULT_MATRIX
+from numba import jit
 
-def get_ordered_block() -> 'Block':
+
+def get_ordered_block():
     return Block(b"".join(i.to_bytes(1) for i in range(64)))
 
 
-class Block:
-    def __init__(self, source_bytes: bytes) -> None:
+class Block(np.ndarray):
+    def __new__(cls, source_bytes: bytes) -> 'Block':
         if not len(source_bytes) == 64:
             raise ValueError(f"Blocks are of length 64 bytes (512 bits) only, supplied length of {len(source_bytes)}")
         
-        self.byte_block = np.ndarray(dtype=np.uint8, shape=(64, 1, 1))
+        byte_block = np.ndarray(dtype=np.uint8, shape=(64, 1, 1)).view(cls)
 
         for i in range(64):
-            self.byte_block[i] = source_bytes[i]
+            byte_block[i] = source_bytes[i]
         
-        self.byte_block = self.byte_block.reshape((4, 4, 4))
+        byte_block = byte_block.reshape((4, 4, 4)).view(cls)
+
+        return byte_block
 
 
     @classmethod
-    def from_byte_block(cls, byte_block: np.ndarray) -> 'Block':
-        obj = object.__new__(cls)
-        obj.byte_block = np.copy(byte_block)
+    def from_byte_block(cls, src_block: 'Block') -> 'Block':
+        obj = src_block.copy()
         return obj
-
-
-    def __str__(self) -> str:
-        return self.byte_block.__str__()
     
 
     def xy_layered_repr(self) -> str:
         int_block = np.ndarray(dtype=np.uint8, shape=(4, 4, 4))
-        for loc, byte in np.ndenumerate(self.byte_block):
+        for loc, byte in np.ndenumerate(self):
             # loc for location, in format x,y,z
             int_block[loc[2]][loc[0]][loc[1]] = byte  # convert to to view layers in the wanted plane 
         return int_block.__str__()
@@ -39,7 +39,7 @@ class Block:
 
     def xz_layered_repr(self) -> str:
         int_block = np.ndarray(dtype=np.uint8, shape=(4, 4, 4))
-        for loc, byte in np.ndenumerate(self.byte_block):
+        for loc, byte in np.ndenumerate(self):
             # loc for location, in format x,y,z
             int_block[loc[1]][loc[0]][loc[2]] = byte  # convert to to view layers in the wanted plane
         return int_block.__str__()
@@ -47,87 +47,125 @@ class Block:
 
     def yz_layered_repr(self) -> str:
         int_block = np.ndarray(dtype=np.uint8, shape=(4, 4, 4))
-        for loc, byte in np.ndenumerate(self.byte_block):
+        for loc, byte in np.ndenumerate(self):
             # loc for location, in format x,y,z
             int_block[loc] = byte  # convert to to view layers in the wanted plane 
         return int_block.__str__()
 
 
+    @jit(nopython=True, nogil=True, cache=True)
     def shift_rows(self) -> None:  # XY layer permutation
-        p_block = self.byte_block
         for z in range(0, 4):
-            p_block[1][0][z], p_block[1][1][z], p_block[1][2][z], p_block[1][3][z] = \
-                p_block[1][1][z], p_block[1][2][z], p_block[1][3][z], p_block[1][0][z]  # row 2
-            p_block[2][0][z], p_block[2][1][z], p_block[2][2][z], p_block[2][3][z] = \
-                p_block[2][2][z], p_block[2][3][z], p_block[2][0][z], p_block[2][1][z]  # row 3
-            p_block[3][0][z], p_block[3][1][z], p_block[3][2][z], p_block[3][3][z] = \
-                p_block[3][3][z], p_block[3][0][z], p_block[3][1][z], p_block[3][2][z]  # row 4
+            self[1][0][z], self[1][1][z], self[1][2][z], self[1][3][z] = \
+                self[1][1][z], self[1][2][z], self[1][3][z], self[1][0][z]  # row 2
+            self[2][0][z], self[2][1][z], self[2][2][z], self[2][3][z] = \
+                self[2][2][z], self[2][3][z], self[2][0][z], self[2][1][z]  # row 3
+            self[3][0][z], self[3][1][z], self[3][2][z], self[3][3][z] = \
+                self[3][3][z], self[3][0][z], self[3][1][z], self[3][2][z]  # row 4
     
 
+    @jit(nopython=True, nogil=True, cache=True)
     def inverse_shift_rows(self) -> None:  # XY layer permutation
-        p_block = self.byte_block
         for z in range(0, 4):
-            p_block[1][0][z], p_block[1][1][z], p_block[1][2][z], p_block[1][3][z] = \
-                p_block[1][3][z], p_block[1][0][z], p_block[1][1][z], p_block[1][2][z]  # row 2
-            p_block[2][0][z], p_block[2][1][z], p_block[2][2][z], p_block[2][3][z] = \
-                p_block[2][2][z], p_block[2][3][z], p_block[2][0][z], p_block[2][1][z]  # row 3
-            p_block[3][0][z], p_block[3][1][z], p_block[3][2][z], p_block[3][3][z] = \
-                p_block[3][1][z], p_block[3][2][z], p_block[3][3][z], p_block[3][0][z]  # row 4
+            self[1][0][z], self[1][1][z], self[1][2][z], self[1][3][z] = \
+                self[1][3][z], self[1][0][z], self[1][1][z], self[1][2][z]  # row 2
+            self[2][0][z], self[2][1][z], self[2][2][z], self[2][3][z] = \
+                self[2][2][z], self[2][3][z], self[2][0][z], self[2][1][z]  # row 3
+            self[3][0][z], self[3][1][z], self[3][2][z], self[3][3][z] = \
+                self[3][1][z], self[3][2][z], self[3][3][z], self[3][0][z]  # row 4
 
 
+    @jit(nopython=True, nogil=True, cache=True)
     def rotate_elements(self) -> None:  # XZ layer permumtation
-        p_block = self.byte_block
         for y in range(0, 4):
-            p_block[0][y][1], p_block[0][y][3], p_block[2][y][3], p_block[2][y][1] = \
-                p_block[2][y][1], p_block[0][y][1], p_block[0][y][3], p_block[2][y][3]  # group 2
-            p_block[1][y][1], p_block[1][y][3], p_block[3][y][3], p_block[3][y][1] = \
-                p_block[3][y][3], p_block[3][y][1], p_block[1][y][1], p_block[1][y][3]  # group 3
-            p_block[1][y][0], p_block[1][y][2], p_block[3][y][2], p_block[3][y][0] = \
-                p_block[1][y][2], p_block[3][y][2], p_block[3][y][0], p_block[1][y][0]  # group 4
+            self[0][y][1], self[0][y][3], self[2][y][3], self[2][y][1] = \
+                self[2][y][1], self[0][y][1], self[0][y][3], self[2][y][3]  # group 2
+            self[1][y][1], self[1][y][3], self[3][y][3], self[3][y][1] = \
+                self[3][y][3], self[3][y][1], self[1][y][1], self[1][y][3]  # group 3
+            self[1][y][0], self[1][y][2], self[3][y][2], self[3][y][0] = \
+                self[1][y][2], self[3][y][2], self[3][y][0], self[1][y][0]  # group 4
     
 
+    @jit(nopython=True, nogil=True, cache=True)
     def inverse_rotate_elements(self) -> None:  # XZ layer permutation
-        p_block = self.byte_block
         for y in range(0, 4):
-            p_block[0][y][1], p_block[0][y][3], p_block[2][y][3], p_block[2][y][1] = \
-                p_block[0][y][3], p_block[2][y][3], p_block[2][y][1], p_block[0][y][1]  # group 2
-            p_block[1][y][1], p_block[1][y][3], p_block[3][y][3], p_block[3][y][1] = \
-                p_block[3][y][3], p_block[3][y][1], p_block[1][y][1], p_block[1][y][3]  # group 3
-            p_block[1][y][0], p_block[1][y][2], p_block[3][y][2], p_block[3][y][0] = \
-                p_block[3][y][0], p_block[1][y][0], p_block[1][y][2], p_block[3][y][2]  # group 4
+            self[0][y][1], self[0][y][3], self[2][y][3], self[2][y][1] = \
+                self[0][y][3], self[2][y][3], self[2][y][1], self[0][y][1]  # group 2
+            self[1][y][1], self[1][y][3], self[3][y][3], self[3][y][1] = \
+                self[3][y][3], self[3][y][1], self[1][y][1], self[1][y][3]  # group 3
+            self[1][y][0], self[1][y][2], self[3][y][2], self[3][y][0] = \
+                self[3][y][0], self[1][y][0], self[1][y][2], self[3][y][2]  # group 4
     
 
+    @jit(nopython=True, nogil=True, cache=True)
     def spin_rings(self) -> None:  # YZ layer permutation
-        p_block = self.byte_block
         for x in range(0, 4):
-            p_block[x][1][2], p_block[x][2][1] = p_block[x][2][1], p_block[x][1][2]  # ring 2
-            p_block[x][1][1], p_block[x][2][2] = p_block[x][2][2], p_block[x][1][1]  # ring 3
-            p_block[x][0][0], p_block[x][0][3], p_block[x][3][0], p_block[x][3][3] = \
-                p_block[x][0][3], p_block[x][3][3], p_block[x][0][0], p_block[x][3][0]  # ring 1
-            p_block[x][0][2], p_block[x][2][3], p_block[x][3][1], p_block[x][1][0] = \
-                p_block[x][1][0], p_block[x][0][2], p_block[x][2][3], p_block[x][3][1]  # ring 4
-            p_block[x][0][1], p_block[x][1][3], p_block[x][3][2], p_block[x][2][0] = \
-                p_block[x][2][0], p_block[x][0][1], p_block[x][1][3], p_block[x][3][2]  # ring 5
+            self[x][1][2], self[x][2][1] = self[x][2][1], self[x][1][2]  # ring 2
+            self[x][1][1], self[x][2][2] = self[x][2][2], self[x][1][1]  # ring 3
+            self[x][0][0], self[x][0][3], self[x][3][0], self[x][3][3] = \
+                self[x][0][3], self[x][3][3], self[x][0][0], self[x][3][0]  # ring 1
+            self[x][0][2], self[x][2][3], self[x][3][1], self[x][1][0] = \
+                self[x][1][0], self[x][0][2], self[x][2][3], self[x][3][1]  # ring 4
+            self[x][0][1], self[x][1][3], self[x][3][2], self[x][2][0] = \
+                self[x][2][0], self[x][0][1], self[x][1][3], self[x][3][2]  # ring 5
             
 
+    @jit(nopython=True, nogil=True, cache=True)
     def inverse_spin_rings(self) -> None:
-        p_block = self.byte_block
         for x in range(0, 4):
-            p_block[x][1][2], p_block[x][2][1] = p_block[x][2][1], p_block[x][1][2]  # ring 2
-            p_block[x][1][1], p_block[x][2][2] = p_block[x][2][2], p_block[x][1][1]  # ring 3
-            p_block[x][0][0], p_block[x][0][3], p_block[x][3][0], p_block[x][3][3] = \
-                p_block[x][3][0], p_block[x][0][0], p_block[x][3][3], p_block[x][0][3]  # ring 1
-            p_block[x][0][2], p_block[x][2][3], p_block[x][3][1], p_block[x][1][0] = \
-                p_block[x][2][3], p_block[x][3][1], p_block[x][1][0], p_block[x][0][2]  # ring 4
-            p_block[x][0][1], p_block[x][1][3], p_block[x][3][2], p_block[x][2][0] = \
-                p_block[x][1][3], p_block[x][3][2], p_block[x][2][0], p_block[x][0][1]  # ring 5
+            self[x][1][2], self[x][2][1] = self[x][2][1], self[x][1][2]  # ring 2
+            self[x][1][1], self[x][2][2] = self[x][2][2], self[x][1][1]  # ring 3
+            self[x][0][0], self[x][0][3], self[x][3][0], self[x][3][3] = \
+                self[x][3][0], self[x][0][0], self[x][3][3], self[x][0][3]  # ring 1
+            self[x][0][2], self[x][2][3], self[x][3][1], self[x][1][0] = \
+                self[x][2][3], self[x][3][1], self[x][1][0], self[x][0][2]  # ring 4
+            self[x][0][1], self[x][1][3], self[x][3][2], self[x][2][0] = \
+                self[x][1][3], self[x][3][2], self[x][2][0], self[x][0][1]  # ring 5
 
 
+    @jit(nopython=True, nogil=True, cache=True)
     def sub_bytes(self) -> None:
-        for loc, byte in np.ndenumerate(self.byte_block):
-            self.byte_block[loc] = S_BOX[byte]
+        for loc, byte in np.ndenumerate(self):
+            self[loc] = S_BOX[byte]
 
     
+    @jit(nopython=True, nogil=True, cache=True)
     def inverse_sub_bytes(self) -> None:
-        for loc, byte in np.ndenumerate(self.byte_block):
-            self.byte_block[loc] = INVERSE_S_BOX[byte]
+        for loc, byte in np.ndenumerate(self):
+            self[loc] = INVERSE_S_BOX[byte]
+    
+
+    def mix_xy_columns(self) -> None:
+        for z in range(0, 4):
+            self[slice(0, 4)][slice(0, 4)][z] = \
+                (XY_MULT_MATRIX @ EXTENTION_FIELD(self[slice(0, 4)][slice(0, 4)][z]))
+    
+
+    def inverse_mix_xy_columns(self) -> None:
+        for z in range(0, 4):
+            self[slice(0, 4)][slice(0, 4)][z] = \
+                (INVERSE_XY_MULT_MATRIX @ EXTENTION_FIELD(self[slice(0, 4)][slice(0, 4)][z]))
+
+
+    def mix_xz_columns(self) -> None:
+        for y in range(0, 4):
+            self[slice(0, 4)][y][slice(0, 4)] = \
+                (XZ_MULT_MATRIX @ EXTENTION_FIELD(self[slice(0, 4)][y][slice(0, 4)]))
+    
+
+    def inverse_mix_xz_columns(self) -> None:
+        for y in range(0, 4):
+            self[slice(0, 4)][y][slice(0, 4)] = \
+                (INVERSE_XZ_MULT_MATRIX @ EXTENTION_FIELD(self[slice(0, 4)][y][slice(0, 4)]))
+    
+
+    def mix_yz_columns(self) -> None:
+        for x in range(0, 4):
+            self[x][slice(0, 4)][slice(0, 4)] = \
+                (YZ_MULT_MATRIX @ EXTENTION_FIELD(self[x][slice(0, 4)][slice(0, 4)]))
+    
+
+    def inverse_mix_yz_columns(self) -> None:
+        for x in range(0, 4):
+            self[x][slice(0, 4)][slice(0, 4)] = \
+                (INVERSE_YZ_MULT_MATRIX @ EXTENTION_FIELD(self[x][slice(0, 4)][slice(0, 4)]))
--- tests/test_block.py
+++ tests/test_block.py
@@ -1,54 +1,102 @@
-from rijndael_statter import Block, get_ordered_block
 import numpy as np
+from rijndael_statter import Block, get_ordered_block
+
+
+def test_repr_compiling() -> None:
+    b = get_ordered_block()
+    b.xy_layered_repr()
+    b.xz_layered_repr()
+    b.yz_layered_repr()
 
 
 def test_circular_shift_rows() -> None:
     b = get_ordered_block()
-    b_start_copy = Block.from_byte_block(b.byte_block)
+    b_start_copy = Block.from_byte_block(b)
 
-    assert np.all(np.equal(b.byte_block, b_start_copy.byte_block))
+    assert np.all(np.equal(b, b_start_copy))
 
     b.shift_rows()
-    assert np.any(np.not_equal(b.byte_block, b_start_copy.byte_block))
+    assert np.any(np.not_equal(b, b_start_copy))
 
     b.inverse_shift_rows()
-    assert np.all(np.equal(b.byte_block, b_start_copy.byte_block))
+    assert np.all(np.equal(b, b_start_copy))
 
 
 def test_circular_rotate_elements() -> None:
     b = get_ordered_block()
-    b_start_copy = Block.from_byte_block(b.byte_block)
+    b_start_copy = Block.from_byte_block(b)
 
-    assert np.all(np.equal(b.byte_block, b_start_copy.byte_block))
+    assert np.all(np.equal(b, b_start_copy))
 
     b.rotate_elements()
-    assert np.any(np.not_equal(b.byte_block, b_start_copy.byte_block))
+    assert np.any(np.not_equal(b, b_start_copy))
 
     b.inverse_rotate_elements()
-    assert np.all(np.equal(b.byte_block, b_start_copy.byte_block))
+    assert np.all(np.equal(b, b_start_copy))
 
 
 def test_circular_spin_rings() -> None:
     b = get_ordered_block()
-    b_start_copy = Block.from_byte_block(b.byte_block)
+    b_start_copy = Block.from_byte_block(b)
 
-    assert np.all(np.equal(b.byte_block, b_start_copy.byte_block))
+    assert np.all(np.equal(b, b_start_copy))
 
     b.spin_rings()
-    assert np.any(np.not_equal(b.byte_block, b_start_copy.byte_block))
+    assert np.any(np.not_equal(b, b_start_copy))
 
     b.inverse_spin_rings()
-    assert np.all(np.equal(b.byte_block, b_start_copy.byte_block))
+    assert np.all(np.equal(b, b_start_copy))
 
 
 def test_circular_sub_bytes() -> None:
     b = get_ordered_block()
-    b_start_copy = Block.from_byte_block(b.byte_block)
+    b_start_copy = Block.from_byte_block(b)
 
-    assert np.all(np.equal(b.byte_block, b_start_copy.byte_block))
+    assert np.all(np.equal(b, b_start_copy))
 
     b.sub_bytes()
-    assert np.any(np.not_equal(b.byte_block, b_start_copy.byte_block))
+    assert np.any(np.not_equal(b, b_start_copy))
 
     b.inverse_sub_bytes()
-    assert np.all(np.equal(b.byte_block, b_start_copy.byte_block))
+    assert np.all(np.equal(b, b_start_copy))
+
+
+def test_circular_mix_xy_columns() -> None:
+    b = get_ordered_block()
+    b_start_copy = Block.from_byte_block(b)
+
+    assert np.all(np.equal(b, b_start_copy))
+
+    b.mix_xy_columns()
+    assert np.any(np.not_equal(b, b_start_copy))
+
+    b.inverse_mix_xy_columns()
+    assert np.all(np.equal(b, b_start_copy))
+
+
+
+def test_circular_mix_xz_columns() -> None:
+    b = get_ordered_block()
+    b_start_copy = Block.from_byte_block(b)
+
+    assert np.all(np.equal(b, b_start_copy))
+
+    b.mix_xz_columns()
+    assert np.any(np.not_equal(b, b_start_copy))
+
+    b.inverse_mix_xz_columns()
+    assert np.all(np.equal(b, b_start_copy))
+
+
+
+def test_circular_mix_yz_columns() -> None:
+    b = get_ordered_block()
+    b_start_copy = Block.from_byte_block(b)
+
+    assert np.all(np.equal(b, b_start_copy))
+
+    b.mix_yz_columns()
+    assert np.any(np.not_equal(b, b_start_copy))
+
+    b.inverse_mix_yz_columns()
+    assert np.all(np.equal(b, b_start_copy))
